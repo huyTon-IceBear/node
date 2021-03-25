b689a008f6ce3a5408e148d8eab61e4a4c11b6fd
huy <479984@student.saxion.nl>
2021-03-25 17:05:27 +0700

    Add measurements

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page baby");
-} );
+});
+
+app.get('/measurements', (req,res) => {
+    res.send("This is where we put the measurements");
+});
 
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log("Wazzzzappp");
-})
+});
 
